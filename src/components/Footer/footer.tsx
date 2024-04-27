6c9dd7f6b48adb5131c28c9b0ac61b60cfac4f06
mardev-K <7mardev@gmail.com>
2024-04-27 18:32:13 +0800

    change domain

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -3,7 +3,7 @@ import styles from './footer.module.css';
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p className={styles.cr}>© CebuNow. All Rights Reserved 2024</p>
+      <p className={styles.cr}>© Mardev. All Rights Reserved 2024</p>
     </footer>
   );
 }
